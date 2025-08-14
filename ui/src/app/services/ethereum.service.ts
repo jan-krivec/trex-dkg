db7351d1f0bd0ab38f70ca2836999ed834bfd41f
jan-krivec <jan.krivec98@gmail.com>
2025-08-14 22:05:51 +0200

    add correct port

--- ui/src/app/services/ethereum.service.ts
+++ ui/src/app/services/ethereum.service.ts
@@ -85,6 +85,7 @@ export class EthereumService implements OnInit, OnDestroy {
     this.dkg = new DKG({
       environment: 'development', // or devnet, testnet, mainnet
       endpoint: 'https://193.2.72.90/node',  // gateway node URI
+      port: "",
       blockchain: {
         name: 'hardhat1:31337', // or otp:2043, base:8453, gnosis:100
         rpc: 'https://193.2.72.90/evm1'
